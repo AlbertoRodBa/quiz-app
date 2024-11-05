7bfba66504f0ef6d7096790401f20a86f010347a
AlbertoRodBa <rodriguez.barrios.alberto@gmail.com>
2024-11-04 22:14:41 -0300

    Agregado ícono de Font Awesome en Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
 
 function Header({ title }) {
   return (
     <header className="header">
-      <h1>{title}</h1>
+      <div className="header-title">
+        <h1>{title}</h1>
+        <FontAwesomeIcon icon={faCircleQuestion} className="question-icon" />
+      </div>
     </header>
   );
 }
